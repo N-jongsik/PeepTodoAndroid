ce4ea21712bbf13a45c8fddbecabbeb38d22310d
N-jongsik <jongsik0625@naver.com>
2024-12-22 02:57:06 +0900

    mypage

--- app/src/main/java/com/peeptodo/peep_todo_android/ui/todopage/fragments/MainPageFragment.kt
+++ app/src/main/java/com/peeptodo/peep_todo_android/ui/todopage/fragments/MainPageFragment.kt
@@ -1,11 +1,13 @@
 package com.peeptodo.peep_todo_android.ui.todopage.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.peeptodo.peep_todo_android.databinding.FragmentTodoMainPageBinding
+import com.peeptodo.peep_todo_android.ui.mypage.MyPageActivity
 
 class MainPageFragment : Fragment() {
 
@@ -17,6 +19,14 @@ class MainPageFragment : Fragment() {
     ): View {
         _binding = FragmentTodoMainPageBinding.inflate(inflater, container, false)
         // binding.view의 id 로 불러서 씀
+
+        binding.textView.setOnClickListener {
+            // MyPageActivity로 이동
+            val intent = Intent(requireContext(), MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return binding.root
     }
 
